4f623ecb0dc04c0e814837e4e693a849c061cfc6
l0st-idi0t <carlnod121@gmail.com>
2022-12-18 23:38:47 -0600

    added projects

--- JS/yeetus.js
+++ JS/yeetus.js
@@ -1,1 +1,20 @@
+fetch("https://api.github.com/users/l0st-idi0t/repos")
+.then(response => response.json())
+    .then(data => {
+        let grid = document.querySelector('.grid');
 
+        for (let i = 0; i < data.length; i++) {
+            let newCell = document.createElement('div');
+            newCell.onclick = () => {
+                location.href = `${data[i].html_url}`;
+            }
+            newCell.classList.add('grid-item');
+
+            newCell.innerHTML = `
+                <h1>${data[i].name}</h1>
+                <p>${data[i].description}</p>
+            `;
+            grid.appendChild(newCell);
+        }
+    }
+);
